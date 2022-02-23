be9e7ecf5334e6f3051a59c215073f868a44655e
Oleksii <>
2022-02-23 21:14:39 +0200

    Updated Vuforia to 10.5.5, Build 0.1.3

--- App/Assets/Scripts/VuforiaHelpers/FocusModeUpdater.cs
+++ App/Assets/Scripts/VuforiaHelpers/FocusModeUpdater.cs
@@ -1,29 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
 public class FocusModeUpdater : MonoBehaviour
 {
-    [SerializeField] private CameraDevice.FocusMode focusMode;
+    [SerializeField] private FocusMode focusMode;
     [SerializeField] private bool isSetOnStart;
 
-    void Start()
+    private void Start()
     {
         UpdateFocusMode(focusMode);
     }
 
     public void UpdateToContinuosAutoFocus()
     {
-        UpdateFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        UpdateFocusMode(FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
     }
 
     public void UpdateToTriggerAutoFocus()
     {
-        UpdateFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+        UpdateFocusMode(FocusMode.FOCUS_MODE_TRIGGERAUTO);
     }
 
-    public void UpdateFocusMode(CameraDevice.FocusMode newFocusMode)
+    public void UpdateFocusMode(FocusMode newFocusMode)
     {
         focusMode = newFocusMode;
         CameraDevice.Instance.SetFocusMode(focusMode);
